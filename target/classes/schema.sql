07b80d535d9126a8e66c702178b5e955d502a67d
Shivkaran Ravidas <s.ravidas01@gmail.com>
2024-02-10 11:48:00 +0530

    code commit
    code commit

--- target/classes/schema.sql
+++ target/classes/schema.sql
@@ -1,6 +1,6 @@
 CREATE  TABLE IF NOT EXISTS products
 (
-    product_id int primary key,
+    product_id varchar(100) primary key,
     title varchar(200),
     description varchar(200),
     price varchar(10),
